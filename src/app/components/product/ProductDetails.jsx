29d73f7c95e6a3c7c0e0815099e6e0cf4d4b1f84
Brahim T <Brahim17@hotmail.fr>
2021-11-02 11:04:54 +0100

    [#04][Feat][BT] - [add comment in ProductDetails.jsx and ProductDetailView.jsx]

--- src/app/components/product/ProductDetails.jsx
+++ src/app/components/product/ProductDetails.jsx
@@ -1,11 +1,22 @@
+import React from "react";
+
+/**
+ * Component product to show details
+ * @param label
+ * @param price
+ * @param description
+ * @returns show details product
+
+ */
 const ProductDetails = ({label, price, description}) => {
     return(
         <>
-            <span className="font-bold">{label}</span>
-            <br/>
-            <span>{price} €</span>
-            <br/>
-            <span>{description}</span>
+            <div className="flex flex-col flex items-center bg-gray-200 mb-2">
+                <div className="font-bold">{label}</div>
+                <div className="pr-2"><span>Prix: </span>{price}€ </div>
+            </div>
+            <div className="rounded-2xl bg-yellow-400 mr-2 ml-2 pl-1 border-double border-2">{description}</div>
+
         </>
     );
 };
--- src/app/views/ProductDetailView.jsx
+++ src/app/views/ProductDetailView.jsx
@@ -3,6 +3,11 @@ import figurine from '../assets/images/figurine.jpg'
 import {productDetail} from "../api/backend/product";
 import ProductDetails from "../components/product/ProductDetails"
 
+/**
+ * recovers the product in databas with the id
+ * @author Brahim TALLA
+ */
+
 const ProductDetailView = () => {
     const [product, setProduct] = useState([])
 
@@ -12,16 +17,16 @@ const ProductDetailView = () => {
         })
     },[])
     return (
-        <>
+        <div className="flex flex-col md:flex-row flex items-center">
                 <img
-                    width={200}
+                    className="w-36 md:w-auto mb-4"
                     src={figurine}
                     alt=""
                 />
                 <ProductDetails label={product.label}
                                 price={product.price}
                                 description={product.description}/>
-        </>
+        </div>
     );
 };
 
