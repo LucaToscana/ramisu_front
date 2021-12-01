d2840d4ef01a5ebf687a0b13c7cb39ea8b85ae4c
Cécile <cecile.baillard@laposte.net>
2021-12-01 15:57:48 +0100

    [13][FEAT][BC] - added icons, polished CSS (not perfect) and changed the app name (index.html)

--- src/app/views/AccountView.jsx
+++ src/app/views/AccountView.jsx
@@ -1,46 +1,111 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { signOut } from '../shared/redux-store/authenticationSlice';
+import { URL_CART } from './../shared/constants/urls/urlConstants';
+import boxes from "../assets/images/icones/boxes.png";
+import userProfile from '../assets/images/icones/user.png';
+import shoppingCart from '../assets/images/icones/shopping-cart.png';
+import heart from '../assets/images/icones/heart.png';
+import creditCard from '../assets/images/icones/credit-card.png';
+import key from '../assets/images/icones/key.png';
+import powerButton from '../assets/images/icones/power-off.png';
 
 
 function classNames(...classes) {
     return classes.filter(Boolean).join(' ')
 }
 
+/**
+ * The user page account with multiple links (cart, user infos, orders...)
+ * 
+ * @returns the AccountView page
+ */
+
 const AccountView = () => {
 
     const navigation = [
-        { name: 'Commandes', href: '#' },
-        { name: 'Données du compte', href: '#' },
-        { name: 'Panier', href: '#' },
-        { name: 'Favoris', href: '#' },
-        { name: 'Moyens de paiement', href: '#' },
-        { name: 'Gestion du mot de passe', href: '#' },
-        { name: 'Déconnexion', href: '#' },
+        { name: 'Commandes', href: '#', image: boxes },
+        { name: 'Données du compte', href: '#', image: userProfile },
+        { name: 'Panier', href: URL_CART, image: shoppingCart },
+        { name: 'Favoris', href: '#', image: heart },
+        { name: 'Moyens de paiement', href: '#', image: creditCard },
+        { name: 'Gestion du mot de passe', href: '#', image: key },
     ]
 
     return (
 
-        <div className="flex flex-col justify-center items-center m-10">
-            <div className="box-border h-70 w-90 p-4 border-4">
+        // Position of the elements below
+        <div className="flex flex-col justify-center items-center m-5">
+            {/* The box container characteristics */}
+            <div className="box-border p-6 border-4 lg:w-3/6 sm:w-1/3 ">
+                {/* Grid system of the box content */}
+                <div className="grid lg:grid-cols-2 sm:grid-cols-1 gap-4 place-content-center">
 
-                <div className="grid grid-cols-2 gap-4 place-content-center">
+                    {/* The different links in the box container */}
                     {navigation.map((item) => (
                         <a
                             key={item.name}
                             href={item.href}
                             className={classNames(
-                                item.current ? 'bg-gray-900 text-black' : 'text-black hover:bg-gray-700 hover:text-white',
-                                'px-3 py-2 rounded-md text-sm font-medium'
+                                'flex flex-col justify-center items-center text-sm font-medium object-contain'
                             )}
-                            aria-current={item.current ? 'page' : undefined}
                         >
-                            {item.name}
+                            {/* Hovering effect */}
+                            <div className='text-black hover:bg-gray-400 hover:text-black px-2 rounded-md object-center'>
+
+                                {/* Grid system for the image and link */}
+                                <div className="grid grid-cols-3 w-40 align-middle">
+
+                                    {/* Image takes up 1 column */}
+                                    <img className="h-50 col-span-1" src={item.image} alt='' />
+                                    {/* Link takes up 2 columns */}
+                                    <div className="col-span-2">
+                                        {item.name}
+                                    </div>
+
+                                </div>
+
+                            </div>
                         </a>
                     ))}
-                </div>
 
+                    <DisconnectionLink />
+
+                </div>
             </div>
         </div>
     )
 }
 
 export default AccountView;
+
+const DisconnectionLink = () => {
+    const dispatch = useDispatch();
+
+    return (
+
+        <a
+            key="Déconnexion"
+            href=" "
+            className={classNames(
+                'flex flex-col justify-center items-center text-sm font-medium object-contain'
+            )}
+            onClick={() => dispatch(signOut())}
+        >
+             {/* Hovering effect */}
+            <div className='text-black hover:bg-gray-400 hover:text-black px-2 rounded-md'>
+
+                {/* Grid system for the image and link */}
+                <div className="grid grid-cols-3 w-40 align-middle">
+
+                    {/* Image takes up 1 column */}
+                    <img className="h-50 col-span-1" src={powerButton} alt='' />
+                    {/* Link takes up 2 columns */}
+                    <div className="col-span-2">
+                        Se déconnecter
+                    </div>
+                </div>
+            </div>
+        </a>
+    )
+}
