baebbb65a40fef68665cd8c33b3dba24cd43321b
Ices63 <brice.bayard@gmail.com>
2022-02-28 15:47:38 +0100

    Update LoginView.jsx

--- src/app/views/LoginView.jsx
+++ src/app/views/LoginView.jsx
@@ -30,7 +30,7 @@ const LoginView = ({ history }) => {
             await recaptchaRef.current.executeAsync().then(token=>{
                 authenticate(values).then(res => {
                     
-                    debugger
+                    // debugger
                     if (res.status === 200 && res.data.token) {
                         dispatch(signIn(res.data.token))
                         
