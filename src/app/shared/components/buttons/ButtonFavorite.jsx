177f5293002f49aed248f74358acd655e44e3582
Malek MOKRANI <malek.mokrani@outlook.com>
2022-03-02 13:06:06 +0100

    loading state

--- src/app/shared/components/buttons/ButtonFavorite.jsx
+++ src/app/shared/components/buttons/ButtonFavorite.jsx
@@ -1,26 +1,21 @@
 import React from 'react';
-import { HeartIcon } from '@heroicons/react/outline';
-import { useState } from 'react';
-import { selectIsLogged } from './../../../shared/redux-store/authenticationSlice';
-import {
-    useSelector,
-    useDispatch
-} from 'react-redux';
-import {
-    addFavorite,
-    removeFavorite
-} from '../../../api/backend/user'
-import { selectorFav, selectorFavState } from '../../../shared/redux-store/favoritesSlice'
-import { useEffect } from 'react';
-import { fetchFav } from '../../redux-store/favoritesSlice'
+import {    HeartIcon, RefreshIcon } from '@heroicons/react/outline';
+import {    useState } from 'react';
+import {    selectIsLogged } from './../../../shared/redux-store/authenticationSlice';
+import {    useSelector , useDispatch  } from 'react-redux';
+import {    addFavorite , removeFavorite    } from '../../../api/backend/user'
+import {    fetchFav    , selectorFav, selectorFavState } from '../../../shared/redux-store/favoritesSlice'
+import {    useEffect } from 'react';
 
 const ButtonFavorite = ({ id }) => {
 
     const [toggle, setToggle] = useState(null);
+    const [reload, setReload] = useState(false);
     const isLogged = useSelector(selectIsLogged);
     const fav = useSelector(selectorFav);
     const favState = useSelector(selectorFavState);
 
+    let lock = true;
     const dispatch = useDispatch();
 
     useEffect(() => {
@@ -30,24 +25,41 @@ const ButtonFavorite = ({ id }) => {
                 if (elt.id === id) bool = true;
                 return;
             });
+            
+          
             setToggle(bool);
         }
     });
 
-    const clickHandler = (event) => {
-        setToggle(!toggle);
+    useEffect(()=>{
+        switch (favState) {
+            case "loading":
+                if(!lock)setReload(true);
+            break;
+            default:
+                lock = true;
+                setReload(false);
+            break;
+        }
+    },[favState]);
+
+    const clickHandler = (event) => {   
+        lock = false;
+        setReload(true)
         if (!toggle) add();
         else rm();
     }
 
     const getDivStyle = () => {
-        const style = "p-2 rounded-2xl m-1 p-3 hover:cursor-pointer";
+        const style = " p-2 rounded-2xl m-1 p-3 hover:cursor-pointer";
+      
         if (toggle) return "bg-[#ff0000] " + style;
         return "login " + style;
 
     }
 
     const add = () => {
+      
         addFavorite(id).then((res) => {
             dispatch(fetchFav())
         }).catch(error => console.log(error))
@@ -63,10 +75,20 @@ const ButtonFavorite = ({ id }) => {
         {!isLogged ? (<></>) : (
             <div className={getDivStyle()}>
                 <button title={toggle ? "Parmi vos produits favoris" : "Ajouter au favoris"} >
-                    <HeartIcon width={32} height={32}
-                        onClick={clickHandler}
-                        className={toggle ? "text-white " : "text-black"} />
-                </button>
+                {
+                     !reload ?
+                     (<HeartIcon 
+                            width={32} 
+                            height={32}
+                            onClick={clickHandler}
+                            className={toggle ? "text-white " : "text-black"} />)
+                    :
+                     (<RefreshIcon 
+                            width={32} 
+                            height={32}
+                            onClick={clickHandler} 
+                            className="scale-[-1] animate-spin" />)
+}                </button>
             </div>)}
     </div>);
 }
