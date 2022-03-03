4f118b9b3f53d4ca45436930ae57178febb670c1
Loic LASKRI <loic.laskri@gmail.com>
2022-03-03 16:59:24 +0100

    Revert "modifs carousel"
    This reverts commit 05e7e7a3a14ad704240f61b46298d1ca70cf1b12.

--- src/app/views/HomeView.jsx
+++ src/app/views/HomeView.jsx
@@ -89,11 +89,11 @@ const HomeView = ({ history }) => {
         <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 cursor-default">
 
             <div id="carouselExampleIndicators" class="carousel slide relative" data-bs-ride="carousel">
-                <div class="carousel-indicators absolute right-0 bottom-40 left-0 flex justify-center p-0 mb-4">
+                <div class="carousel-indicators absolute right-0 bottom-70 left-0 flex justify-center p-0 mb-4">
                 <button
                 type="button"
                 data-bs-target="#carouselExampleIndicators"
-                data-bs-slide-to="20"
+                data-bs-slide-to="0"
                 class="active"
                 aria-current="true"
                 aria-label="Slide 1"
@@ -114,28 +114,28 @@ const HomeView = ({ history }) => {
                 <div class="carousel-inner relative w-full overflow-hidden">
                 <div class="carousel-item active float-left w-full">
                 <img
-                    src="../assets/images/carousel01.jpg"
+                    src="..\assets\images\carousel01"
                     class="block w-full"
                     alt="Warhammer001"
                 />
                 </div>
                 <div class="carousel-item float-left w-full">
                 <img
-                    src="../assets/images/carousel02.jpg"
+                    src="..\assets\images\carousel02"
                     class="block w-full"
                     alt="Warhammer002"
                 />
                 </div>
                 <div class="carousel-item float-left w-full">
                 <img
-                    src="../assets/images/carousel03.jpg"
+                    src="..\assets\images\carousel03"
                     class="block w-full"
                     alt="Warhammer003"
                 />
                 </div>
                 </div>
                 <button
-                class="carousel-control-prev absolute top-10 bottom-15 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0"
+                class="carousel-control-prev absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0"
                 type="button"
                 data-bs-target="#carouselExampleIndicators"
                 data-bs-slide="prev"
@@ -144,7 +144,7 @@ const HomeView = ({ history }) => {
                 <span class="visually-hidden">Previous</span>
                 </button>
                 <button
-                class="carousel-control-next absolute top-10 bottom-15 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0"
+                class="carousel-control-next absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0"
                 type="button"
                 data-bs-target="#carouselExampleIndicators"
                 data-bs-slide="next"
@@ -153,7 +153,7 @@ const HomeView = ({ history }) => {
                 <span class="visually-hidden">Next</span>
                 </button>
             </div>
-
+            
             <div className='text-primary-500 font-extrabold'>
                 {loadingRandom ? < Loader /> :
                     <>
