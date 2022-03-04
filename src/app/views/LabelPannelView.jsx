6ef5aec7ec218aab4c2e15dae77e7e9327ee0940
Ices63 <brice.bayard@gmail.com>
2022-03-04 16:30:20 +0100

    affichage des labels

--- src/app/views/LabelPannelView.jsx
+++ src/app/views/LabelPannelView.jsx
@@ -1,66 +1,28 @@
 import React, { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
 import { getAllCategories, getAllUniverses, getAllTags } from '../api/backend/filter';
-import { selectProductFilter } from '../shared/redux-store/filterProductSlice';
 
 /**
- * The Panel of Universes, Categories and ?? Tags ?? controle
- *
- * @returns the LabelPannelView page
+ * The LabelPannelView page
+ * 
+ * The Panel of Universes, Categories and Tags controle
+ * 
+ * @author Brice Bayard
  */
 
 const LabelPannelView = () => {
-  /* ************************* Importer depuis ProductView *********************************** */
-  const filterStore = useSelector(selectProductFilter);
-  const getFIlter = () => {
-
-    let f = []
-
-    if (filterStore.tag !== null) {
-
-        for (let i = 0; i <= filterStore.tag.length - 1; i++) {
-            f.push(filterStore.tag[i]);
-        }
-    }
-    if (filterStore.universe !== null) {
-
-        for (let i = 0; i <= filterStore.universe.length - 1; i++) {
-            f.push(filterStore.universe[i]);
-        }
-    }
-
-    if (filterStore.category !== null) {
-
-        for (let i = 0; i <= filterStore.category.length - 1; i++) {
-            f.push(filterStore.category[i]);
-        }
-    }
-
-
-    return f
-
-  }
-
-  const [filters, setFilters] = useState(getFIlter());
-        const handleFilters = (label) => {
-        if (filters.includes(label)) {
-            setFilters(filter => [...filter].filter((filter) => filter !== label))
-
-        } else {
-            setFilters(filter => [...filter, label]);
-
-
-        }
-    }
   
-  /* ************************** Importer depuis SideBarFilters ********************************** */
+  // stock the items retrieved by the back in list
   const [categories, setCategories] = useState([]);
   const [universes, setUniverses] = useState([]);
   const [tags, setTags] = useState([]);
 
-  // Allows to recover data in back
+  // switch visibility of label type
+  const [showCategories, setShowCategories] = useState(false);
+  const [showUniverses, setShowUniverses] = useState(false);
+  const [showTags, setShowTags] = useState(false);
+
+  // Recover data in back
   useEffect(() => {
-    //   dispatch( initFilter())
     getAllCategories().then(responses => {
         setCategories(responses.data)})
 
@@ -72,26 +34,48 @@ const LabelPannelView = () => {
     
   }, []);
 
-  const listUniverses = universes.map(universe => {
+  // stock the style to DRY x3
+  const styleListPannel = "w-40 p-1 my-2 md:space-x-5 xl:space-x-10 bg-custom-orange border-2 border-gray-300 focus:bg-yellow-400 rounded-xl whitespace-nowrap overflow-hidden overflow-ellipsis";
+  
+  // return a stylised button of each label type
+  const listCategories = categories.map(categorie => {
     return (
-      <button className="w-40 p-1 my-2 bg-custom-orange border-2 border-gray-300 focus:bg-yellow-400 rounded-xl whitespace-nowrap overflow-hidden overflow-ellipsis">
-      <span> Label - 2 </span>
+      <button className={styleListPannel} alt={categorie.label} title={categorie.label} key={"categorie"+categorie.id}>
+      <span> {categorie.label} </span>
       </button>
     )
-  }); // TODO faire afficher les noms des universes (prend en exemple dans SideBarFilters)
+  });
+  const listUniverses = universes.map(universe => {
+    return (
+      <button className={styleListPannel} alt={universe.label} title={universe.label} key={"universe"+universe.id}>
+      <span> {universe.label} </span>
+      </button>
+    )
+  });
+  const listTags = tags.map(tag => {
+    return (
+      <button className={styleListPannel} alt={tag.label} title={tag.label} key={"tag"+tag.id}>
+      <span> {tag.label} </span>
+      </button>
+    )
+  });
+
+  // stock the style to DRY x6
+  const buttonPannelStyle = "w-40 mt-5 bg-custom-orange border-2 border-gray-300 focus:bg-yellow-400 rounded-xl";
 
   return (
     // Position of the elements below
     <div  className="flex flex-col justify-center items-center mx-6 md:mx-20 xl:mx-40 text-white">
-      {/* The box container characteristics */}
+      {/* The box container */}
       <div className="box-border m-6 border-4 w-full min-h-[500px] bg-gray-700">
         <div className="text-center mt-5">
           <h2 className="underline font-bold">Pannel de gestion :</h2>
         </div>
+        {/* Button to hide each label type separately */}
         <div className="md:flex justify-center text-center md:space-x-5 xl:space-x-10 mt-5">
-          <button className="w-40 p-5 mt-5 bg-custom-orange border-2 border-gray-300 focus:bg-yellow-400 rounded-xl">Catégories</button>
-          <button className="w-40 p-5 mt-5 bg-custom-orange border-2 border-gray-300 focus:bg-yellow-400 rounded-xl">Univers</button>
-          <button className="w-40 p-5 mt-5 bg-custom-orange border-2 border-gray-300 focus:bg-yellow-400 rounded-xl">Tags</button>
+          <button className={buttonPannelStyle+" p-5"}  onClick={() => setShowCategories(!showCategories)}>Catégories</button>
+          <button className={buttonPannelStyle+" p-5"} onClick={() => setShowUniverses(!showUniverses)}>Univers</button>
+          <button className={buttonPannelStyle+" p-5"} onClick={() => setShowTags(!showTags)}>Tags</button>
         </div>
         <div className="text-center mt-10">
           <div className="border-2 border-gray-300 rounded-xl w-min sm:w-96 p-3 mx-auto">
@@ -99,16 +83,19 @@ const LabelPannelView = () => {
             <input type="text" aria-label="inputPannel" className="rounded-md border-gray-300 bg-gray-500 placeholder-gray-400" placeholder="Label name" />
           </div>
           <div className="md:flex justify-center text-center md:space-x-5 xl:space-x-10">
-            <button className="w-40 p-1 mt-5 bg-custom-orange border-2 border-gray-300 focus:bg-yellow-400 rounded-xl">Ajouter</button>
-            <button className="w-40 p-1 mt-5 bg-custom-orange border-2 border-gray-300 focus:bg-yellow-400 rounded-xl">Modifier</button>
-            <button className="w-40 p-1 mt-5 bg-custom-orange border-2 border-gray-300 focus:bg-yellow-400 rounded-xl">Supprimer</button>
+            <button className={buttonPannelStyle+" p-1"}>Ajouter</button>
+            <button className={buttonPannelStyle+" p-1"}>Modifier</button>
+            <button className={buttonPannelStyle+" p-1"}>Supprimer</button>
           </div>
           <div className="justify-center mt-20">
             <div>
               <h3>Les labels existants :</h3>
             </div>
-            <div className="md:flex m-5 md:space-x-5 xl:space-x-10 border-2 border-gray-300 rounded-xl justify-center">
-              {listUniverses}
+            {/* recover all label type in the box with possibility to hide them */}
+            <div className="m-5 border-2 border-gray-300 rounded-xl justify-center">
+              <span hidden={!showCategories}> {listCategories} </span>
+              <span hidden={!showUniverses}> {listUniverses} </span>
+              <span hidden={!showTags}> {listTags} </span>
             </div>
           </div>
         </div>
