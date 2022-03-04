3cbce6308de06dc8f81938e0fd735c23895a866c
Malek MOKRANI <malek.mokrani@outlook.com>
2022-03-04 21:00:22 +0100

    Refactoring : rename variable and setter [explicite]

--- src/app/views/ProductView.jsx
+++ src/app/views/ProductView.jsx
@@ -16,8 +16,7 @@ const ProductView = () => {
  
     useEffect(() => {
 
-        dispatch(labelFilter(""))
-
+        dispatch(labelFilter(""));
         document.getElementById("searchNavBar").value = ""
 
          }, []);
@@ -55,17 +54,14 @@ const ProductView = () => {
     // Creation of a state for products
     const [openModal, setOpenModal] = useState(false);
     const [filters, setFilters] = useState(getFIlter());
-    const [show, setShow] = useState(true);
+    const [displayGrid, setLayout] = useState(true);
 
 
     const handleFilters = (label) => {
         if (filters.includes(label)) {
             setFilters(filter => [...filter].filter((filter) => filter !== label))
-
         } else {
             setFilters(filter => [...filter, label]);
-
-
         }
     }
 
@@ -73,66 +69,52 @@ const ProductView = () => {
 
     return (
         <div>
-            <ShowFilters
-                filters={filters}
-                handleFilters={handleFilters}
-            />
-            <div className="flex justify-between lg:justify-end m-4">
-                <div className="pl-2 pr-2 filterButton w-2/5 flex justify-between items-center lg:hidden" onClick={() => setOpenModal(!openModal)}>
-                    <button className="font-bold">
-                        Filtre
-                    </button>
-                    <AdjustmentsIcon
-                        className="transform rotate-90"
-                        width={32}
-                        height={32}
-                    />
-                </div>
-                {openModal &&
-                    <div className="fixed z-10 left-0 top-auto bg-gray-300 w-screen h-screen object-contain lg:hidden" aria-modal="true" aria-labelledby="modal-title" role="dialog">
-                        <SideBarFilters
-                            toggleModal={openModal}
-                            setToggleModal={setOpenModal}
-                            filters={filters}
-                            handleFilters={handleFilters}
-                        />
-                        <div className="lg:hidden flex justify-center ">
-                            <button className="filterButton p-2 mt-10" onClick={() => setOpenModal(!openModal)} >
-                                Fermer
-                            </button>
-                        </div>
+            <ShowFilters    filters={filters}   handleFilters={handleFilters}/>
+                <div className="flex justify-between lg:justify-end m-4">
+                    
+                    <div className="pl-2 pr-2 filterButton w-2/5 flex justify-between items-center lg:hidden" onClick={() => setOpenModal(!openModal)}>
+                        <button className="font-bold">Filtre</button>
+                        <AdjustmentsIcon    className="transform rotate-90" width={32} height={32}  />
                     </div>
+                
+                {
+                    openModal &&
+                        <div    className="fixed z-10 left-0 top-auto bg-gray-300 w-screen h-screen object-contain lg:hidden" aria-modal="true" 
+                                aria-labelledby="modal-title" role="dialog">
+                            <SideBarFilters toggleModal={openModal} setToggleModal={setOpenModal}   
+                                            filters={filters}   handleFilters={handleFilters}   />
+                            <div className="lg:hidden flex justify-center ">
+                                <button className="filterButton p-2 mt-10" 
+                                        onClick={() => setOpenModal(!openModal)} >
+                                    Fermer
+                                </button>
+                            </div>
+                        </div>
                 }
+                
                 <div className="flex justify-center self-center m-2 ">
                     <p>{"total products:  " + JSON.parse(localStorage.getItem("filters")).total}</p>
                 </div>
-                <div className="bg-gray-200 text-sm text-gray-500 leading-none border-2 border-gray-200 rounded-full inline-flex">
-                    <button onClick={() => setShow(true)} className={`inline-flex items-center transition-colors duration-300 ease-in focus:outline-none  focus:text-blue-400 rounded-l-full px-4 py-2 ${show ? "active" : ""}`} id="grid">
-                        <ViewGridIcon
-                            width={32}
-                            height={32}
-                        />
+                
+                <div className="hidden md:block bg-gray-200 text-sm text-gray-500 leading-none border-2 border-gray-200 rounded-full inline-flex">
+
+                    <button onClick={() => setLayout(true)} className={`inline-flex items-center transition-colors duration-300 ease-in focus:outline-none  focus:text-blue-400 rounded-l-full px-4 py-2 ${displayGrid ? "active" : ""}`} id="grid">
+                        <ViewGridIcon width={32}  height={32} />
                     </button>
-                    <button onClick={() => setShow(false)} className={`inline-flex items-center transition-colors duration-300 ease-in focus:outline-none  focus:text-blue-400 rounded-l-full px-4 py-2 ${show ? "" : "active"}`} id="list">
-                        <ViewListIcon
-                            width={32}
-                            height={32}
-                        />
+                
+                    <button onClick={() => setLayout(false)} className={`inline-flex items-center transition-colors duration-300 ease-in focus:outline-none  focus:text-blue-400 rounded-l-full px-4 py-2 ${displayGrid ? "" : "active"}`} id="list">
+                        <ViewListIcon width={32} height={32} />
                     </button>
+                
                 </div>
             </div>
 
             <div className="lg:flex">
                 <div className="hidden lg:grid w-52 filter lg:m-5 p-1">
-                    <SideBarFilters
-                        handleFilters={handleFilters}
-                        filters={filters}
-                    />
+                    <SideBarFilters handleFilters={handleFilters} filters={filters} />
                 </div>
                 <div className='w-full'>
-                <ListProducts
-                    show={show}
-                /></div>
+                    <ListProducts   displayGrid={displayGrid} /></div>
             </div>
 
         </div>
