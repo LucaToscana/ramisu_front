5c668c62d7604e7c690a12e55fcd36b144b67b49
Malek MOKRANI <malek.mokrani@outlook.com>
2022-03-08 13:57:09 +0100

    New registerFrom [short registration]

--- src/app/components/account/Register.jsx
+++ src/app/components/account/Register.jsx
@@ -4,9 +4,7 @@ import { defaulValuesRegistration } from '../../shared/constants/formik-yup/defa
 import { schemaFormRegistration } from '../../shared/constants/formik-yup/yup/yupUser';
 import ErrorMessSmall from '../../shared/components/form-and-error-components/ErrorMessSmall';
 import {  CustomInput } from '../../shared/components/form-and-error-components/InputCustom';
-import AddressInput from '../../shared/components/form-and-error-components/AddressInput';
-import DatePickerField from '../../shared/components/form-and-error-components/DatePickerField';
-import "react-datepicker/dist/react-datepicker.css";
+
 
 
 
@@ -21,118 +19,40 @@ import "react-datepicker/dist/react-datepicker.css";
  * @author Peter Mollet / Luca Toscana / Malek Mokrani
  */
 
-const RegistrationFrom = ({ submit, errorLog }) => (
+const RegistrationFrom = ({ submit, errorMsg  }) => (
 
 
     <Formik enableReinitialize={true}
-            initialValues={defaulValuesRegistration} 
-            onSubmit={submit} 
-            validationSchema={schemaFormRegistration}
-            >
+    initialValues={defaulValuesRegistration} 
+    onSubmit={submit} 
+    validationSchema={schemaFormRegistration}
+    >
         <Form className='m-8'>
             <div className='rounded-md shadow-sm -space-y-px'>
-                <label className="mb-2">Nom</label>
+                <label className="mb-2">Votre nom</label>
                 <Field type="text" name="lastName"
-                    component={CustomInput} className='rounded-none rounded-t-md mb-4 shadow-inner'  noError/>
+                    component={CustomInput} className='rounded-none rounded-t-md mb-4 shadow-inner'  />
 
 
-                <label className="mb-2">Prenom</label>
+                <label className="mb-2">Votre prénom</label>
                 <Field type='text' name='firstName'
-                    component={CustomInput} className='rounded-none rounded-b-md mb-4 shadow-inner'  noError/>
-
-
-                <label className="mb-2">Anniversaire</label>
-                <DatePickerField name="birthdate" />
-
-
-
-
-                <label className="mb-2">Mail</label>
+                    component={CustomInput} className='rounded-none rounded-b-md mb-4 shadow-inner'  />
+             
+                <label className="mb-2">Email</label>
+                {errorMsg && <ErrorMessSmall middle message={errorMsg} />}
                 <Field type="text" name="mail"
-                    component={CustomInput} className='rounded-none rounded-t-md mb-4 shadow-inner' noError />
-
-
-                <label className="mb-2">
-                    <div className='flex '>
-                        <div className='p-1'>
-
-                            <svg className="h-4 w-4 text-black" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-                            </svg>
-
-                        </div><div>   Adresse</div> </div> </label>
-
-                <div className='grid grid-cols-1 justify-items-center	'>
-
-                    <AddressInput></AddressInput>
-
-                </div>
-
-                <div className='grid grid-cols-3  gap-2	'>
-                    <div className='grid grid-cols-1 justify-items-center	'>
-
-                        <label className="mb-2">Numero Rue</label>
-                        <Field  type="text" name="number"
-                                component={CustomInput} 
-                                className='rounded-none rounded-t-md mb-4 shadow-inner' noError />
-                    </div>
-
-                    <div className='grid grid-cols-1 justify-items-stretch col-span-2 w-full'>
-                        <label className="mb-2">Rue</label>
-                        <Field  type='text' name='street'
-                                component={CustomInput} 
-                                className=' rounded-none rounded-b-md mb-4 shadow-inner' noError />
-                    </div>
-
-                    <div className='grid grid-cols-1 justify-items-center'>
-                        <label className="mb-2">Code Postal</label>
-                        <Field  type='text' name='postalCode'
-                                component={CustomInput} 
-                                className='rounded-none rounded-b-md mb-4 shadow-inner' noError />
-                    </div>
-
-                    <div    className='grid grid-cols-1 justify-items-center	'>
-                            <label className="mb-2">Ville</label>
-                            <Field  type="text" name="city"
-                                    component={CustomInput} 
-                                    className='rounded-none rounded-t-md mb-4 shadow-inner' noError />
-                    </div>
-
-                    <div className='grid grid-cols-1 justify-items-center'>
-                        <label className="mb-2">Pays</label>
-                        <Field  type='text' name='country'
-                                component={CustomInput} 
-                                className='rounded-none rounded-b-md mb-4 shadow-inner' noError  />
-                    </div>
-                </div>
-
-
-                <div className='grid grid-cols-1 justify-items-center	justify-items-stretch '>
-
-                    <label className="mb-2">Complement Address</label>
-                    <Field  type='text' 
-                            name='additionalAddress'
-                            component={CustomInput} 
-                            className='rounded-none rounded-b-md mb-4 shadow-inner' noError />
-                </div>
-
-
-                <label className="mb-2">Telephone</label>
-                <Field  type='text' name='phone'
-                        component={CustomInput} 
-                        className='rounded-none rounded-b-md mb-4 shadow-inner' noError />
-
-
-                <label className="mb-2">Password</label>
+                    component={CustomInput} className='rounded-none rounded-t-md mb-4 shadow-inner' />
+                <label className="mb-2">Mot de passe</label>
                 <Field type='password' name='password'
                         component={CustomInput} 
-                        className='rounded-none rounded-b-md mb-4 shadow-inner' noError />
+                        placeholder="8 charactères minimum"
+                        className='rounded-none rounded-b-md mb-4 shadow-inner'  />
 
 
-                <label className="mb-2">Password Test</label>
+                <label className="mb-2">Entrez le mot de passe à nouveau</label>
                 <Field type='password' name='passwordTest'
                         component={CustomInput} 
-                        className='rounded-none rounded-b-md mb-4 shadow-inner' noError />
+                        className='rounded-none rounded-b-md mb-4 shadow-inner'  />
             </div>
 
 
@@ -145,7 +65,7 @@ const RegistrationFrom = ({ submit, errorLog }) => (
                     S'inscrire
                 </button>
             </div>
-            {/*????????email*/errorLog && <ErrorMessSmall middle message="Identifiants incorrect(s)" />}
+         
         </Form>
     </Formik>
 )
@@ -170,7 +90,7 @@ const Register = (props) => {
         <div className='mt-10 md:w-1/2 md:border-2 md:shadow-2xl'>
            
                 <h2 className="text-center font-bold uppercase md:mt-8">
-                    Inscription
+                    Créer un compte
                 </h2>
             
 
